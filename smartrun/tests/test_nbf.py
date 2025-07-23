2ea7862e696b02cd2aa8a33ff0d1d501b93baacd
Sermet Pekin <sermet.pekin@gmail.com>
2025-07-24 00:16:55 +0300

    tests jupyter

--- smartrun/tests/test_nbf.py
+++ smartrun/tests/test_nbf.py
@@ -1,9 +1,7 @@
-
-# python .\smartrun\cli.py install a.json
+import nbformat as nbf
 from smartrun.options import Options
 from smartrun.cli import CLI
 from dataclasses import dataclass
-import sys
 
 
 @dataclass
@@ -15,7 +13,6 @@ class Args:
     html: bool = False
     exc: str | None = None
     inc: str | None = None
-
 
 def helper(args):
     opts = Options(
@@ -29,17 +26,19 @@ def helper(args):
     )
     CLI(opts).router()
 
+def test_generate_notebook(tmp_path):
+    import nbformat
 
-def t1():
-    args = Args("venv", ".venv")
+    nb = nbformat.v4.new_notebook()
+    nb.cells.append(nbformat.v4.new_code_cell("import nbformat;from rich import print;print('Hello from notebook')"))
+    notebook_file = tmp_path / "test.ipynb"
+
+    with open(notebook_file, "w") as f:
+        nbformat.write(nb, f)  # ✅ use top-level nbformat.write
+
+    assert notebook_file.exists()
+
+    args = Args(str(notebook_file))
     return helper(args)
 
 
-def t2():
-    args = Args("install", "pandas,rich;nbformat")
-    return helper(args)
-
-def test_main():
-    t1()
-    t2()
-    assert True 
