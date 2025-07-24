8a95305ec3d41ffd246e1229d35ba8c58b23007b
Sermet Pekin <sermet.pekin@gmail.com>
2025-07-24 19:31:09 +0300

    docs html_build dir remove

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 from sphinx.util import logging
 import os
-html_build_dir = "../docs"  # Directory for HTML build output
+# html_build_dir = "../docs"  # Directory for HTML build output
 project = "smartrun"
 copyright = "2025, Sermet Pekin"
 author = "Sermet Pekin"
