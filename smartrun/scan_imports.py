31de5b5ac6308d4bd9cd46adbeab248e7e8e30a4
Sermet Pekin <sermet.pekin@gmail.com>
2025-09-01 22:23:29 +0300

    polishing

--- smartrun/scan_imports.py
+++ smartrun/scan_imports.py
@@ -152,7 +152,6 @@ def create_extra_requirements(packages: list, opts: Options):
     create_requirements_file(file_name, content)
 
 
-
 def scan_imports_file(file_path: str, opts: Options) -> PackageSet:
     file_path = Path(file_path)
     # Get problematic module names and build exclusion list
